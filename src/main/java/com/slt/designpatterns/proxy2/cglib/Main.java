b227610a3cb387c1cf51dc6827f732584634af5d
yuyanan <yuyanan@handongkeji.com>
2020-10-27 18:12:38 +0800

    2

--- src/main/java/com/slt/designpatterns/proxy2/cglib/Main.java
+++ src/main/java/com/slt/designpatterns/proxy2/cglib/Main.java
@@ -1,48 +1,56 @@
-//package com.slt.designpatterns.proxy2.cglib;
-//
-//import net.sf.cglib.proxy.Enhancer;
-//import net.sf.cglib.proxy.MethodInterceptor;
-//import net.sf.cglib.proxy.MethodProxy;
-//
-//import java.lang.reflect.Method;
-//import java.util.Random;
-//
-///**
-// * CGLIB实现动态代理不需要接口 底层也是asm
-// */
-//public class Main {
-//    public static void main(String[] args) {
-//        Enhancer enhancer = new Enhancer();
-//        enhancer.setSuperclass(Tank.class);
-//        enhancer.setCallback(new TimeMethodInterceptor());
-//        Tank tank = (Tank)enhancer.create();
-//        tank.move();
-//    }
-//}
-//
-//class TimeMethodInterceptor implements MethodInterceptor {
-//
-//    @Override
-//    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-//
-//        System.out.println(o.getClass().getSuperclass().getName());
-//        System.out.println("before");
-//        Object result = null;
-//        result = methodProxy.invokeSuper(o, objects);
-//        System.out.println("after");
-//        return result;
-//    }
-//}
-//
-//class Tank {
-//    public void move() {
-//        System.out.println("Tank moving claclacla...");
-//        try {
-//            Thread.sleep(new Random().nextInt(10000));
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
-//
-//
+package com.slt.designpatterns.proxy2.cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+import java.lang.reflect.Method;
+import java.util.Random;
+
+/**
+ * CGLIB实现动态代理不需要接口 底层也是asm
+ * 如果类 是final 就无法使用 cglib
+ */
+public class Main {
+    public static void main(String[] args) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Tank.class);
+        enhancer.setCallback(new TimeMethodInterceptor());
+        //动态代理对象 生成
+        Tank tank = (Tank)enhancer.create();
+        tank.move();
+        tank.fire();
+    }
+}
+
+class TimeMethodInterceptor implements MethodInterceptor {
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println(o.getClass().getSuperclass().getName());
+        System.out.println("before");
+        Object result = null;
+        result = methodProxy.invokeSuper(o, objects);
+        System.out.println("after");
+        return result;
+    }
+}
+
+class Tank {
+    public void move() {
+        System.out.println("Tank moving claclacla...");
+        try {
+            Thread.sleep(new Random().nextInt(10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void fire() {
+        System.out.println("Tank fire duang...");
+        try {
+            Thread.sleep(new Random().nextInt(10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
